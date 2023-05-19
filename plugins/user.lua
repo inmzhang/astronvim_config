a5a58d9e5d5df74ce6e4c1dfd35f55232d10f7fb
inm <zhangyiming21@mail.ustc.edu.cn>
2023-05-19 12:22:54 +0800

    add more plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,5 @@
 return {
+
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
@@ -28,5 +29,32 @@ return {
       },
       panel = { enabled = true },
     },
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+  {
+    "chikko80/error-lens.nvim",
+    event = "BufRead",
+    dependencies = {
+      "nvim-telescope/telescope.nvim"
+    },
+  },
+  {
+    "ggandor/leap.nvim",
+    event = 'BufRead',
+    dependencies = {
+      'tpope/vim-repeat',
+    },
+    config = function()
+      require('leap').add_default_mappings()
+    end,
   },
 }
