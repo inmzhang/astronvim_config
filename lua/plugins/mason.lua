1d7866bb629cf8ad0211398ed624164116cd2f9e
Yiming Zhang <zhangyiming21@mail.ustc.edu.cn>
2024-07-11 21:16:29 +0800

    turn on some default lsp/formatter

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,7 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- Customize Mason plugins
-
 ---@type LazySpec
 return {
   -- use mason-lspconfig to configure LSP installations
@@ -12,8 +8,6 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "marksman",
-        "lua_ls",
-        "ruff_lsp",
         "typst_lsp",
         -- add more arguments for adding more language servers
       })
@@ -28,7 +22,7 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "prettier",
         "stylua",
-        "typst_fmt",
+        "typstfmt",
         -- add more arguments for adding more null-ls sources
       })
     end,
@@ -39,7 +33,6 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "python",
         -- add more arguments for adding more debuggers
       })
     end,
