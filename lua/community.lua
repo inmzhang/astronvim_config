4aae94d89cf74a287af39d060a628883b05aff9c
Yiming Zhang <zhangyiming21@mail.ustc.edu.cn>
2025-03-02 17:46:45 +0800

    remove typscript pack and add cmake

--- lua/community.lua
+++ lua/community.lua
@@ -9,10 +9,12 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.pack.rust" },
+  -- { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.typst" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.cmake" },
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.search.nvim-hlslens" },
